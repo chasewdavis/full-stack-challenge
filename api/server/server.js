831186beb6d762eae705cc82aba0d8fe7884ab92
Chase Davis <chasewdavis7@gmail.com>
2018-05-17 15:32:43 -0700

    post endpoint test ready

--- api/server/server.js
+++ api/server/server.js
@@ -1,57 +1,112 @@
 require('dotenv').config();
 
 const express = require('express'),
-      request = require('superagent');
+      request = require('superagent'),
+      bodyParser = require('body-parser'),
+      _ = require('lodash');
 
 // normally should include .env inside .gitignore
 const ACME_API_KEY = process.env.ACME_API_KEY;  
-
 const app = express();
+
+app.use(bodyParser.json());
 
 app.post('/order/:make/:model/:package/:customer_id', (req,res) => {
 
-    const { make, model, package, customer_id } = req.params
+    let { make, model, package, customer_id } = req.params
+    const id = customer_id;
 
-    console.log('make', make);
-    console.log('model', model);
-    console.log('package', package);
-    console.log('customer_id', customer_id);
-
-    // what acme has available
-    let models = ['anvil','wile','roadrunner'];
-    let packages = ['std','super','elite'];
+    const supplies = {
+        acme : {
+            make: 'acme',
+            models: ['anvil','wile','roadrunner'],
+            packages: ['std','super','elite']
+        },
+        rainier: {
+            make: 'rainier',
+            models: ['pugetsound','olympic'],
+            packages: ['mtn','ltd','14k']
+        }
+    }
 
     let model_available;
     let package_available;
-
-    if(make === 'acme'){
+    const UNAVAILABLE = 'UNAVAILABLE';
+ 
+    function whatsAvailable(models, packages){
         if(models.includes(model)){
             model_available = true;
         }
         if(packages.includes(package)){
             package_available = true;
         }
+    }
+
+    // client asked for valid make but invalid model and / or package
+    // let client know what is not available
+    function notAvailableResponse(model, package){
+        model = model_available ? model : UNAVAILABLE;
+        package = package_available ? package : UNAVAILABLE;
+        res.status(200).send({make,model,package,id});
+    }
+
+    if(make === 'acme'){
+
+        const { models, packages } = supplies.acme;
+
+        whatsAvailable( models, packages );
+
         if(model_available && package_available){
 
-            console.log('both available, making request');
-
             request.post(`http://localhost:3050/acme/api/v45.1/order/${ACME_API_KEY}/${model}/${package}`)
-                .end( (err, res) => {
-                        // console.log('err: ', err);
-                        console.log('res: ', res.data);
+                .end( (err, acme_res) => {
+                    // console.log('err: ', err);
+                    console.log('acme_res: ', acme_res.text);
                 }) 
-        }else{
-            // let client know what is not available
-            // one the other or neither
+                
+        } else {
+            notAvailableResponse(model, package)
         }
 
     } if (make === 'rainier'){
-        request.post('http://localhost:3051/')
+
+        const { models, packages } = supplies.rainier;
+
+        whatsAvailable(models,packages);
+
+        if(model_available && package_available){
+
+            request.post(`http://localhost:3050/acme/api/v45.1/order/${ACME_API_KEY}/${model}/${package}`)
+                .end( (err, acme_res) => {
+                // console.log('err: ', err);
+                console.log('rainer_res: ', acme_res.text);
+                }) 
+
+        } else {
+            notAvailableResponse(model, package)
+        }
+
+    } else {
+        // the client requested for an unavailable supplier, but
+        // maybe the client chose a valid the model or package 
+ 
+        _.forIn(supplies, (val, key) => {
+
+            if( val.models.includes(model) ){
+                model_available = val.make
+            }
+            if( val.packages.includes(package) ){
+                package_available = val.make
+            }
+        })
+
+        res.status(200).send({
+            make: UNAVAILABLE,
+            model: model_available,
+            package: package_available
+        })
     }
 
-    setTimeout( () => {
-        return res.status(200).send({ make: make })
-    }, 2000)
 })
 
 PORT = 3030;
